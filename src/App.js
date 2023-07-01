95a04b81d61bee0e428caa82814b0d0013704156
Gwendal <Gwendal.lefloch82@gmail.com>
2023-07-01 10:49:15 +0200

    api

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const TFTTable = () => {
       for (let i = 0; i < newUsers.length; i++) {
         const user = newUsers[i];
         let avg = 0;
-        const ranking = await axios.get('https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/'+user.id+'?api_key=RGAPI-e1470ccc-a7ec-47fd-b1ed-725716a2adb4');
+        const ranking = await axios.get('https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/'+user.id+'?api_key=RGAPI-7634fff9-4dc8-4f2e-9a58-c131290404dd');
         
         if (ranking.data[0]) {
           const data = ranking.data[0];
